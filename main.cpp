2a3bf2501ce28ac754cdd1d38b5e15515d9154c1
jQ-0 <jerreteq@gmail.com>
2025-06-10 20:13:12 -0500

    Got some basic stuff going.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 #include <SDL3/SDL.h>
 
-
 int main() {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = nullptr;
+    SDL_Renderer *renderer = nullptr;
+    SDL_CreateWindowAndRenderer("wassup",
+        800, 600, 0, &window, &renderer);
+
+    bool quit = false;
+    while (!quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) quit = true;
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_Quit();
     return 0;
     }
